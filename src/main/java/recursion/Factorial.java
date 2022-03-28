45355a1111d24dbaab70184654883011e87a34f6
mahe606 <mahe606@gmail.com>
2022-03-28 08:48:36 +0530

    Committed from intellij on 28 March

--- src/main/java/recursion/Factorial.java
+++ src/main/java/recursion/Factorial.java
@@ -2,10 +2,7 @@ package recursion;
 
 public class Factorial {
     public static void main(String[] args) {
-        System.out.println(factorial(1));
-        System.out.println(factorial(2));
-        System.out.println(factorial(3));
-        System.out.println(factorial(4));
+        System.out.println(factorial(10));
     }
 
     public static int factorial(int n){
