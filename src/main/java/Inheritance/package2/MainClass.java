376a3bd493e4c40d95658763c6c5fc230e7de0d1
mahe606 <mahe606@gmail.com>
2022-07-15 10:07:33 +0530

    Committed from intellij on 28 March

---
+++ src/main/java/Inheritance/package2/MainClass.java
@@ -0,0 +1,14 @@
+package Inheritance.package2;
+
+import Inheritance.package1.Parent;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Parent parent = new Parent();
+
+        // only public method can be called
+        parent.m4();
+    }
+
+
+}
